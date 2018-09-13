f736b009e85eaba6460916903e787851f2c6d24b
Emmanuel Aguilar <emmanuel1aguilars@gmail.com>
2018-09-13 17:34:43 -0500

    contactos

---
+++ contactos/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace contactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TXTNOMA = new System.Windows.Forms.TextBox();
+            this.TXTTLOCAL = new System.Windows.Forms.TextBox();
+            this.TXTCEL = new System.Windows.Forms.TextBox();
+            this.TXTEMAIL = new System.Windows.Forms.TextBox();
+            this.TXTOCEL = new System.Windows.Forms.TextBox();
+            this.TXTOEMAIL = new System.Windows.Forms.TextBox();
+            this.mas = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.AGREGAR = new System.Windows.Forms.Button();
+            this.REMOVER = new System.Windows.Forms.Button();
+            this.LIMPIAR = new System.Windows.Forms.Button();
+            this.SALIR = new System.Windows.Forms.Button();
+            this.LIBRETA = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(8, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE Y APELLIDOS ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(33, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TELEFONO LOCAL ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(81, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CELULAR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DimGray;
+            this.label4.Location = new System.Drawing.Point(81, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E MAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(334, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CELULAR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DimGray;
+            this.label6.Location = new System.Drawing.Point(345, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "E MAIL ";
+            // 
+            // TXTNOMA
+            // 
+            this.TXTNOMA.Location = new System.Drawing.Point(159, 43);
+            this.TXTNOMA.Name = "TXTNOMA";
+            this.TXTNOMA.Size = new System.Drawing.Size(100, 20);
+            this.TXTNOMA.TabIndex = 6;
+            // 
+            // TXTTLOCAL
+            // 
+            this.TXTTLOCAL.Location = new System.Drawing.Point(159, 76);
+            this.TXTTLOCAL.Name = "TXTTLOCAL";
+            this.TXTTLOCAL.Size = new System.Drawing.Size(100, 20);
+            this.TXTTLOCAL.TabIndex = 7;
+            // 
+            // TXTCEL
+            // 
+            this.TXTCEL.Location = new System.Drawing.Point(159, 107);
+            this.TXTCEL.Name = "TXTCEL";
+            this.TXTCEL.Size = new System.Drawing.Size(100, 20);
+            this.TXTCEL.TabIndex = 8;
+            // 
+            // TXTEMAIL
+            // 
+            this.TXTEMAIL.Location = new System.Drawing.Point(158, 142);
+            this.TXTEMAIL.Name = "TXTEMAIL";
+            this.TXTEMAIL.Size = new System.Drawing.Size(100, 20);
+            this.TXTEMAIL.TabIndex = 9;
+            // 
+            // TXTOCEL
+            // 
+            this.TXTOCEL.Location = new System.Drawing.Point(408, 114);
+            this.TXTOCEL.Name = "TXTOCEL";
+            this.TXTOCEL.Size = new System.Drawing.Size(100, 20);
+            this.TXTOCEL.TabIndex = 10;
+            // 
+            // TXTOEMAIL
+            // 
+            this.TXTOEMAIL.Location = new System.Drawing.Point(408, 149);
+            this.TXTOEMAIL.Name = "TXTOEMAIL";
+            this.TXTOEMAIL.Size = new System.Drawing.Size(100, 20);
+            this.TXTOEMAIL.TabIndex = 11;
+            // 
+            // mas
+            // 
+            this.mas.Location = new System.Drawing.Point(302, 109);
+            this.mas.Name = "mas";
+            this.mas.Size = new System.Drawing.Size(26, 23);
+            this.mas.TabIndex = 12;
+            this.mas.Text = "+";
+            this.mas.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // AGREGAR
+            // 
+            this.AGREGAR.BackColor = System.Drawing.Color.Black;
+            this.AGREGAR.ForeColor = System.Drawing.Color.DimGray;
+            this.AGREGAR.Location = new System.Drawing.Point(48, 208);
+            this.AGREGAR.Name = "AGREGAR";
+            this.AGREGAR.Size = new System.Drawing.Size(75, 23);
+            this.AGREGAR.TabIndex = 14;
+            this.AGREGAR.Text = "AGREGAR";
+            this.AGREGAR.UseVisualStyleBackColor = false;
+            this.AGREGAR.Click += new System.EventHandler(this.AGREGAR_Click);
+            // 
+            // REMOVER
+            // 
+            this.REMOVER.BackColor = System.Drawing.Color.Black;
+            this.REMOVER.ForeColor = System.Drawing.Color.DimGray;
+            this.REMOVER.Location = new System.Drawing.Point(183, 208);
+            this.REMOVER.Name = "REMOVER";
+            this.REMOVER.Size = new System.Drawing.Size(75, 23);
+            this.REMOVER.TabIndex = 15;
+            this.REMOVER.Text = "REMOVER";
+            this.REMOVER.UseVisualStyleBackColor = false;
+            // 
+            // LIMPIAR
+            // 
+            this.LIMPIAR.BackColor = System.Drawing.Color.Black;
+            this.LIMPIAR.ForeColor = System.Drawing.Color.DimGray;
+            this.LIMPIAR.Location = new System.Drawing.Point(302, 208);
+            this.LIMPIAR.Name = "LIMPIAR";
+            this.LIMPIAR.Size = new System.Drawing.Size(75, 23);
+            this.LIMPIAR.TabIndex = 16;
+            this.LIMPIAR.Text = "LIMPIAR";
+            this.LIMPIAR.UseVisualStyleBackColor = false;
+            // 
+            // SALIR
+            // 
+            this.SALIR.BackColor = System.Drawing.Color.Black;
+            this.SALIR.ForeColor = System.Drawing.Color.DimGray;
+            this.SALIR.Location = new System.Drawing.Point(418, 208);
+            this.SALIR.Name = "SALIR";
+            this.SALIR.Size = new System.Drawing.Size(75, 23);
+            this.SALIR.TabIndex = 17;
+            this.SALIR.Text = "SALIR";
+            this.SALIR.UseVisualStyleBackColor = false;
+            // 
+            // LIBRETA
+            // 
+            this.LIBRETA.FormattingEnabled = true;
+            this.LIBRETA.Location = new System.Drawing.Point(48, 247);
+            this.LIBRETA.Name = "LIBRETA";
+            this.LIBRETA.Size = new System.Drawing.Size(445, 95);
+            this.LIBRETA.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(569, 396);
+            this.Controls.Add(this.LIBRETA);
+            this.Controls.Add(this.SALIR);
+            this.Controls.Add(this.LIMPIAR);
+            this.Controls.Add(this.REMOVER);
+            this.Controls.Add(this.AGREGAR);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.mas);
+            this.Controls.Add(this.TXTOEMAIL);
+            this.Controls.Add(this.TXTOCEL);
+            this.Controls.Add(this.TXTEMAIL);
+            this.Controls.Add(this.TXTCEL);
+            this.Controls.Add(this.TXTTLOCAL);
+            this.Controls.Add(this.TXTNOMA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Contactos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TXTNOMA;
+        private System.Windows.Forms.TextBox TXTTLOCAL;
+        private System.Windows.Forms.TextBox TXTCEL;
+        private System.Windows.Forms.TextBox TXTEMAIL;
+        private System.Windows.Forms.TextBox TXTOCEL;
+        private System.Windows.Forms.TextBox TXTOEMAIL;
+        private System.Windows.Forms.Button mas;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AGREGAR;
+        private System.Windows.Forms.Button REMOVER;
+        private System.Windows.Forms.Button LIMPIAR;
+        private System.Windows.Forms.Button SALIR;
+        private System.Windows.Forms.ListBox LIBRETA;
+    }
+}
+
